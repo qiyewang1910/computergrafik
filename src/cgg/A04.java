236c27d0084142326f3837803986c4b559f60759
qiwa1910 <qiwa1910@bht-berlin.de>
2025-11-30 23:36:28 +0100

    a04-1

--- src/cgg/A04.java
+++ src/cgg/A04.java
@@ -62,8 +62,8 @@ public class A04 {
         }
 
         // 创建立方体
-        Quader box = new Quader(6, new Color(0.9, 0.1, 0.1)); // 6x6x6正方体，红色
-        Mat44 boxTrans = Mat44.translate(-5, 3, -62); // Y=3：底部贴地面（6/2=3）             
+        Quader box = new Quader(24, new Color(0.9, 0.1, 0.1)); // 6x6x6正方体，红色
+        Mat44 boxTrans = Mat44.translate(-1, 0.6, -54); // Y=3：底部贴地面（6/2=3）             
         box.setTransform(boxTrans);
         scene.add(box);
 
